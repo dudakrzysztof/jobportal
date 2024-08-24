d0d2046c237f90e9e28f1ccb80218125bfc8c266
KrzysztofDuda <duda_krzysztof@o2.pl>
2024-08-24 11:50:19 +0200

    Initial commit

---
+++ src/test/java/com/github/dudakrzysztof/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dudakrzysztof.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
